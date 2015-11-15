a6d14f2c2eae20eba277ebab33d27e8ec6e1f308
kevin <kevinvarela@opendeusto.es>
2015-11-15 19:03:36 +0100

    prota y enemigo

---
+++ src/Paq/Personajes/Enemigo.java
@@ -0,0 +1,6 @@
+package Paq.Personajes;
+
+public class Enemigo {
+
+
+}
---
+++ src/Paq/Personajes/Prota.java
@@ -0,0 +1,6 @@
+package Paq.Personajes;
+
+public class Prota {
+	
+	//Se puede hacer una opcion para elegir varios tipos de prota, cada uno con sus habilidades, pero eso para el final
+}
