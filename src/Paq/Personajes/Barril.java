565f259f6b5588de642abec17e2353f226d28a87
kevinvarela <kevinvarela@.opendeusto.es>
2015-11-16 10:42:56 +0100

    barril y municion

---
+++ src/Paq/Personajes/Barril.java
@@ -0,0 +1,23 @@
+package Paq.Personajes;
+
+import Paq.Personajes.Objetos.Imagenmov;
+
+public class Barril extends Objetos{
+	
+	private boolean explotar;
+
+	public Barril(String nombre,Imagenmov nombreimagen,boolean explotar) {
+		super(nombre, nombreimagen);
+		Imagenmov Imagencontrolsmov = new Imagenmov("barril.png");
+		Imagencontrolsmov.setSize(420/3,420/3 );
+	}
+	
+	
+	private boolean explosion() {
+		// terminar metodo explotar cuando tengamos "metodo disparar"
+		return true;
+		
+	}
+	
+
+}
---
+++ src/Paq/Personajes/Municion.java
@@ -0,0 +1,11 @@
+package Paq.Personajes;
+
+public class Municion extends Objetos {
+	
+
+	public Municion(String nombre, Imagenmov nombreimagen) {
+		super(nombre, nombreimagen);
+
+	}
+
+}
