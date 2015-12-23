b50e15513516517792b59b11e844f2d14152af35
kevin <kevinvarela@opendeusto.es>
2015-12-23 14:38:20 +0100

    ++

--- src/Paq/Personajes/Bala.java
+++ src/Paq/Personajes/Bala.java
@@ -2,7 +2,7 @@ package Paq.Personajes;
 
 import Paq.Paneles.Escenario;
 
-public class Bala {
+public class  Bala{
 	private JLabelBala miGrafico;
 	protected double posX;  // Posición en X (horizontal)
 	protected double posY;  // Posición en Y (vertical)
@@ -44,6 +44,28 @@ public class Bala {
 
 	public void setA(Escenario a) {
 		this.a = a;
+	}
+	
+	public boolean hayChoqueHorizontalIzquierda( Bala mibala) {
+		return (mibala.getPosX() > a.panelPrincipal.getWidth() - 50
+				- JLabelBala.ANCHURA_BALA / 2
+				- JLabelBala.RADIO_ESFERA_BALA);
+	}
+
+	public boolean hayChoqueHorizontalDerecha( Bala mibala) {
+		return (mibala.getPosX() < JLabelProta.RADIO_ESFERA_PERSONAJE + 50
+				- JLabelBala.ANCHURA_BALA / 2);
+	}
+	
+	public boolean hayChoqueVerticalAbajo(Prota miProta) {
+		return (miProta.getPosY() > a.panelPrincipal.getHeight() - 50
+				- JLabelBala.TAMANYO_BALA / 2
+				- JLabelBala.RADIO_ESFERA_BALA);
+	}
+
+	public boolean hayChoqueVerticalArriba(Prota miProta) {
+		return (miProta.getPosY() < JLabelProta.RADIO_ESFERA_PERSONAJE + 50
+				- JLabelBala.TAMANYO_BALA / 2);
 	}
 
 }
--- src/Paq/Personajes/JLabelBala.java
+++ src/Paq/Personajes/JLabelBala.java
@@ -1,14 +1,19 @@
 package Paq.Personajes;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
+
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
 public class JLabelBala extends JLabel {
 	private static final long serialVersionUID = 1L;  // Para serialización
-	public static final int TAMANYO_PERSONAJE= 20;  // píxels 
-	public static final int ANCHURA_PERSONAJE= 20;
-	public static final int RADIO_ESFERA_PERSONAJE= 10;  // Radio en píxels del bounding circle del prota (para golpes)
-	private static final boolean DIBUJAR_ESFERA_PERSONAJE = true;  // Dibujado (para depuración) del bounding circle de choque del coche
+	public static final int TAMANYO_BALA= 20;  // píxels 
+	public static final int ANCHURA_BALA= 20;
+	public static final int RADIO_ESFERA_BALA= 10;  // Radio en píxels del bounding circle del prota (para golpes)
+	private static final boolean DIBUJAR_ESFERA_BALA = true;  // Dibujado (para depuración) del bounding circle de choque del coche
 	
 	/** Construye y devuelve el JLabel del prota con su gráfico y tamaño
 	 */
@@ -19,8 +24,22 @@ public class JLabelBala extends JLabel {
 			System.err.println( "Error en carga de recurso: coche.png no encontrado" );
 			e.printStackTrace();
 		}
-		setBounds( 0, 0, TAMANYO_PERSONAJE, ANCHURA_PERSONAJE );
-		setSize(TAMANYO_PERSONAJE, ANCHURA_PERSONAJE);
+		setBounds( 0, 0, TAMANYO_BALA, ANCHURA_BALA );
+		setSize(TAMANYO_BALA, ANCHURA_BALA);
+	}
+	
+	protected void paintComponent(Graphics g) {
+//		super.paintComponent(g);   // En este caso no nos sirve el pintado normal de un JLabel
+		Image img = ((ImageIcon)getIcon()).getImage();
+		Graphics2D g2 = (Graphics2D) g;  // El Graphics realmente es Graphics2D
+		// Escalado más fino con estos 3 parámetros:
+		g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+		g2.setRenderingHint(RenderingHints.KEY_RENDERING,RenderingHints.VALUE_RENDER_QUALITY);
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);	
+        // Dibujado de la imagen
+        g2.drawImage( img, 0, 0, TAMANYO_BALA, ANCHURA_BALA, null );
+        if (DIBUJAR_ESFERA_BALA) g2.drawOval( TAMANYO_BALA/2-RADIO_ESFERA_BALA, ANCHURA_BALA/2-RADIO_ESFERA_BALA,
+        		RADIO_ESFERA_BALA*2, RADIO_ESFERA_BALA*2 );
 	}
 
 }
